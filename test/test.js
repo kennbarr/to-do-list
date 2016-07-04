b7176771f1620a76a5bc65c9f30750f4b0a146e0
Carlos <chdezmar@gmail.com>
2016-07-04 17:05:32 +0100

    one test passing

--- test/test.js
+++ test/test.js
@@ -1,9 +1,30 @@
-var assert = require('chai').assert;
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+var path = require('path');
+var expect = require('chai');
+
+const Browser = require('zombie');
+const http = require('http');
+
+describe('Page Load', function() {
+
+  before(function() {
+    this.browser = new Browser({site: 'http://localhost:3000'});
+    var createServer = require("http-server").createServer;
+    var server = createServer({ root: path.join(__dirname, "../") });
+    server.listen(3000);
   });
+
+  before(function(done) {
+    this.browser.visit('/', done);
+  });
+
+  it("page exists", function(){
+    this.browser.assert.success();
+  });
+
+  it("has content", function(){
+    expect(this.browser).to.contain('TO DO List');
+  });
+
+
+
 });
