53ad333ebd16d3fee8601c34f70693e95de91a32
Oliver Diamond <diamond.oliver@gmail.com>
2016-07-05 17:02:10 +0100

    Wrote Ajax Test

--- test/test.js
+++ test/test.js
@@ -13,49 +13,66 @@ describe('Page Load', function() {
     var server = createServer({ root: path.join(__dirname, "../") });
     server.listen(3000);
   });
+  
 
-  beforeEach(function(done) {
-    this.browser.visit('/', done);
+  describe('Task Functionality', function() {
+    beforeEach(function(done) {
+      this.browser.visit('/?json=test', done);
+    });
+    
+    
+    it("page exists", function(){
+      this.browser.assert.success();
+    });
+
+
+    it("can add a task to to-do list", function(){
+      addTask(this.browser);
+      expect(this.browser.text('html')).to.include('New task');
+    });
+
+    it("can mark a task as completed", function(){
+      addTask(this.browser);
+      this.browser.pressButton('Completed');
+      expect(this.browser.html('#list-container')).to.include("completed-task");
+    });
+
+    it('can filter out completed tasks', function() {
+      addTask(this.browser);
+      this.browser.pressButton('Completed');
+      this.browser.pressButton('show-outstanding')
+      expect(this.browser.html('#list-container')).to.include("hidden-task");
+    });
+
+    it('can filter out uncompleted tasks', function() {
+      addTask(this.browser);
+      this.browser.pressButton('show-completed')
+      expect(this.browser.html('#list-container')).to.include("hidden-task");
+    });
+
+    it('shows all tasks', function() {
+      addTask(this.browser);
+      this.browser.pressButton('Completed');
+      this.browser.pressButton('show-outstanding')
+      addTask(this.browser);
+      this.browser.pressButton('show-completed')
+      this.browser.pressButton('show-all')
+      expect(this.browser.html('#list-container')).to.not.include("hidden-task");
+    });
   });
 
-  it("page exists", function(){
-    this.browser.assert.success();
+  describe('Ajax', function(){
+    beforeEach(function(done) {
+      this.browser.visit('/', done);
+    });
+    
+    it('retrieves Marys stuff by default', function() {
+      expect(this.browser.html('#list-container')).to.include("Go to the bank");
+      expect(this.broswer.html('#task-0')).to.include('active-task');
+    });
+
   });
 
-
-  it("can add a task to to-do list", function(){
-    addTask(this.browser);
-    expect(this.browser.text('html')).to.include('New task');
-  });
-
-  it("can mark a task as completed", function(){
-    addTask(this.browser);
-    this.browser.pressButton('Completed');
-    expect(this.browser.html('#list-container')).to.include("completed-task");
-  });
-
-  it('can filter out completed tasks', function() {
-    addTask(this.browser);
-    this.browser.pressButton('Completed');
-    this.browser.pressButton('show-outstanding')
-    expect(this.browser.html('#list-container')).to.include("hidden-task");
-  });
-
-  it('can filter out uncompleted tasks', function() {
-    addTask(this.browser);
-    this.browser.pressButton('show-completed')
-    expect(this.browser.html('#list-container')).to.include("hidden-task");
-  });
-
-  it('shows all tasks', function() {
-    addTask(this.browser);
-    this.browser.pressButton('Completed');
-    this.browser.pressButton('show-outstanding')
-    addTask(this.browser);
-    this.browser.pressButton('show-completed')
-    this.browser.pressButton('show-all')
-    expect(this.browser.html('#list-container')).to.not.include("hidden-task");
-  });
 
 });
 
